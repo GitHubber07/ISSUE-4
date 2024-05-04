4b69f88825a899530904d711c89bb8dd10544246
unknown <chetasivtrivedi@gmail.com>
2024-05-04 11:31:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import PokemonFinder from './compo'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <PokemonFinder></PokemonFinder>
+    </div>
+  )
+}
+
+export default App
